7dae31cf2d2575c73e364c7769ca037fb0c7bc02
Marco Junior <jksolucoesti@gmail.com>
2025-09-27 05:09:30 -0300

    Ajuste do tamanho da area de leitura do barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BarcodeScannerComponent } from './shared/barcode-scanner/barcode-scanner.component';
 
 @NgModule({
   imports: [   
     RouterOutlet,
     BrowserAnimationsModule   
   ],
-  declarations: [                 
-  
-    BarcodeScannerComponent
+  declarations: [                     
   ]
 })
 export class AppModule { }
