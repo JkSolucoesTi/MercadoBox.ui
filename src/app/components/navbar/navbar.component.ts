79719641ba5f9e59f26a3816238bffb1d7136475
Marco Junior <jksolucoesti@gmail.com>
2025-10-02 08:01:42 -0300

    Teste de tela de carrinhos - inserir item

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -37,6 +37,11 @@ export class NavbarComponent {
       label: 'Compras',
       icon: 'pi pi-wallet',
       routerLink: '/compras'
+    },
+    {
+      label: 'Carrinhos',
+      icon: 'pi pi-wallet',
+      routerLink: '/carrinho/CB458AAD-71C8-4DFF-A8F7-E1D47ADBDD8F'
     }
   ];
 
